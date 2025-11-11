0dafdfef270eed45e4e3d302268a0a10235f2299
Khoi110 <khoind9017@ut.edu.vn>
2025-11-11 14:03:56 +0700

    thêm code

---
+++ qlthuoc (1).sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 04, 2025 lúc 05:42 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `qlthuoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `danhmuc`
+--
+
+CREATE TABLE `danhmuc` (
+  `MaDanhMuc` int(11) NOT NULL,
+  `TenDanhMuc` varchar(100) NOT NULL,
+  `MoTa` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`MaDanhMuc`, `TenDanhMuc`, `MoTa`) VALUES
+(1, 'Thuốc', 'Các loại thuốc và sản phẩm chăm sóc sức khỏe'),
+(2, 'Dược mỹ phẩm', 'Mỹ phẩm có tính dược học'),
+(3, 'Thuốc làm đẹp', 'Thuốc làm đẹp cho nữ');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `MaSanPham` int(11) NOT NULL,
+  `TenSanPham` varchar(255) NOT NULL,
+  `GiaGoc` decimal(10,2) NOT NULL,
+  `GiaKhuyenMai` decimal(10,2) DEFAULT NULL,
+  `PhanTramGiam` int(11) DEFAULT NULL,
+  `SoLuongCon` int(11) DEFAULT NULL,
+  `MaDanhMuc` int(11) DEFAULT NULL,
+  `HinhAnh` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`MaSanPham`, `TenSanPham`, `GiaGoc`, `GiaKhuyenMai`, `PhanTramGiam`, `SoLuongCon`, `MaDanhMuc`, `HinhAnh`) VALUES
+(1, 'Nước tẩy trang Bioderma Sensibio', 535000.00, 398750.00, 25, 20, 2, 'bioderma.jpg'),
+(2, 'Tăm chỉ nha khoa Okamura 734503', 22000.00, 18000.00, 18, 20, 3, 'okamura.jpg'),
+(3, 'Nước súc miệng Listerine Cool Mint', 169000.00, 81000.00, 52, 20, 3, 'listerine.jpg'),
+(4, 'Nước Yến Collagen Green Bird Nutrient', 30000.00, 22500.00, 25, 60, 1, 'greenbird.jpg');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  ADD PRIMARY KEY (`MaDanhMuc`);
+
+--
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`MaSanPham`),
+  ADD KEY `MaDanhMuc` (`MaDanhMuc`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  MODIFY `MaDanhMuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `MaSanPham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaDanhMuc`) REFERENCES `danhmuc` (`MaDanhMuc`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
